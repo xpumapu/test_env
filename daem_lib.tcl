85745f0ca391a40a1cdf76ea3b6f4230c7d83e47
Marek Puzyniak <marek.puzyniak@tieto.com>
2016-03-16 14:52:38 +0100

    update netns command
    Signed-off-by: Marek Puzyniak <marek.puzyniak@tieto.com>

--- daem_lib.tcl
+++ daem_lib.tcl
@@ -13,18 +13,21 @@
 
 proc get_ns_ifaces {gap ns_name} {
 	set ns_fd [dict get $gap netns_p $ns_name]
-	return [ns_ifaces [ns_get_ifaces $ns_fd]]
+	return [ns_get_ifaces $ns_fd]
 }
 
-proc check_netns_p {gap} {
+proc get_netns_p {gap} {
 	upvar #0 $gap global_app_p
 	if {[dict exists $global_app_p netns_p]} {
-		return [dict keys [dict get $global_app_p netns_p]]
+		return [dict get $global_app_p netns_p]
 	}
 	;# netns_p not filled yet
 	dict set global_app_p netns_p [ns_get_netns_p]
-	return [dict keys [dict get $global_app_p netns_p]]
+	return [dict get $global_app_p netns_p]
 }
 
-
-
+proc get_netns_list {gap} {
+	upvar #0 $gap global_app_p
+	set netns_p [get_netns_p global_app_p]
+	return [dict keys $netns_p]
+}
